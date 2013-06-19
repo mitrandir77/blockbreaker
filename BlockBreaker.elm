5f12221d84b06aedab9303bd203e7becac59ae61
Mateusz J. Kwapich <mitrandir77@0x04.net>
2013-06-19 23:48:57 +0200

    odbijany

--- BlockBreaker.elm
+++ BlockBreaker.elm
@@ -1,20 +1,38 @@
 import Keyboard
 
 
-player = { angle=0, score=0 }
+player = { angle=0, score=0, x=300, y=0, ball={x=0, y=0, vx=2, vy=2} }
 
 clearGrey = rgb 100 100 200
 playerColor  = rgb 100 200 100
 
 playerPos angle  = (300 * cos angle, 300 * sin angle)
 
-movePlayer {x} p= { p | angle <- p.angle + x/20 }
+movePlayer {x} p=
+    let newAngle = p.angle + x/20
+        (newX, newY) = playerPos newAngle
+    in { p | angle <- newAngle, x <- newX, y<-newY }
 
-step (t, dir) = movePlayer dir
+moveBall t p =
+    let moveBall b t = { b | x <- b.x + b.vx*t , y <- b.y+ b.vy*t }
+    in { p | ball <- moveBall p.ball t }
+
+near k c n = n >= k-c && n <= k+c
+within ball player = (ball.x |> near player.x 8)
+                  && (ball.y |> near player.y 20)
+
+
+detectCollision p =
+    let revertedBall b = {b | vx <- 0-b.vx, vy<-0-b.vy }
+    in   if within p.ball p then {p | ball <- revertedBall p.ball } else p
+
+step (t, dir) = movePlayer dir . moveBall t . detectCollision
 
 
 render p = collage 700 700
-       [ filled clearGrey (circle 300), rect 10 40 |> filled playerColor |> move (playerPos p.angle) |> rotate p.angle ]
+       [ filled clearGrey (circle 300),
+         rect 14 45 |> filled playerColor |> move (p.x, p.y) |> rotate p.angle,
+         circle 10 |> filled white |> move (p.ball.x, p.ball.y)]
 
 
 input = let delta = lift (\t -> t/20) (fps 50)
