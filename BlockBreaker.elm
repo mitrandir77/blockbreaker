3f8c9631d436888402bc81799c2e73c5522e606a
Krzysztof Zajac <kazet@0x04.net>
2013-06-20 14:56:55 +0200

    multiple balls

--- BlockBreaker.elm
+++ BlockBreaker.elm
@@ -1,38 +1,44 @@
 import Keyboard
 
-
-player = { angle=0, score=0, x=300, y=0, ball={x=0, y=0, vx=2, vy=2} }
+player = { angle=0, score=0, x=300, y=0, balls=[{x=0, y=0, vx=1, vy=0}, {x=3, y=3, vx=0, vy=1}] }
 
 clearGrey = rgb 100 100 200
 playerColor  = rgb 100 200 100
 
 playerPos angle  = (300 * cos angle, 300 * sin angle)
 
-movePlayer {x} p=
+movePlayer {x} p =
     let newAngle = p.angle + x/20
         (newX, newY) = playerPos newAngle
     in { p | angle <- newAngle, x <- newX, y<-newY }
 
-moveBall t p =
-    let moveBall b t = { b | x <- b.x + b.vx*t , y <- b.y+ b.vy*t }
-    in { p | ball <- moveBall p.ball t }
-
 near k c n = n >= k-c && n <= k+c
+
 within ball player = (ball.x |> near player.x 8)
                   && (ball.y |> near player.y 20)
 
-
-detectCollision p =
+detectCollision p ball =
     let revertedBall b = {b | vx <- 0-b.vx, vy<-0-b.vy }
-    in   if within p.ball p then {p | ball <- revertedBall p.ball } else p
+    in if within ball p then revertedBall ball else ball
 
-step (t, dir) = movePlayer dir . moveBall t . detectCollision
+detectCollisions p =
+    let d b = detectCollision p b
+    in {p | balls <- map d p.balls }
 
+moveBall b t = { b | x <- b.x + b.vx*t , y <- b.y+ b.vy*t }
 
-render p = collage 700 700
-       [ filled clearGrey (circle 300),
-         rect 14 45 |> filled playerColor |> move (p.x, p.y) |> rotate p.angle,
-         circle 10 |> filled white |> move (p.ball.x, p.ball.y)]
+moveBalls t p =
+    let m b = moveBall b t
+    in {p | balls <- map m p.balls }
+
+step (t, dir) = movePlayer dir . moveBalls t . detectCollisions
+
+render p =
+    let drawBall ball = (circle 10 |> filled white |> move (ball.x, ball.y))
+        balls = map drawBall p.balls
+        board = [filled clearGrey (circle 300)]
+        pad = [rect 14 45 |> filled playerColor |> move (p.x, p.y) |> rotate p.angle]
+    in collage 700 700 (board ++ pad ++ balls)
 
 
 input = let delta = lift (\t -> t/20) (fps 50)
